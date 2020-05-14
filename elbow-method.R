548c790e9645868496f4abff1b3cdece5a9e8a79
m-shaker <>
2020-05-14 15:38:37 -0400

    R code for the elbow method

---
+++ elbow-method.R
@@ -0,0 +1,15 @@
+
+### Choosing the number of clusters using the elbow method 
+
+#Choosing the number of clusters using the elbow method
+library(ggplot2)
+library(purrr)
+
+ggplot(data = data.frame(t(cstats.table(gower.dist, divisive.clust, 15))), 
+       aes(x=cluster.number, y=within.cluster.ss)) + 
+  geom_point()+
+  geom_line()+
+  ggtitle("Divisive clustering") +
+  labs(x = "Num.of clusters", y = "Within clusters sum of squares (SS)") +
+  theme(plot.title = element_text(hjust = 0.5))
+
